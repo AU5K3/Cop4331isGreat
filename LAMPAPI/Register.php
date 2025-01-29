ab336ba1b49b6cf1843c4218beea9185f36af6ac
aga <calkowska.aga@gmail.com>
2025-01-29 14:40:54 -0500

    Made whitespace consistent

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -19,8 +19,7 @@
 
     // connect to database
     $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
-    if( $conn->connect_error )
-    {
+    if ($conn->connect_error) {
         returnWithError("Database connection failed: " . $conn->connect_error ); // failed to connect
         exit();
     }
@@ -35,8 +34,7 @@
 
     $result = $stmt->get_result();
 
-    if ( $result->num_rows != 0 ) 
-    {
+    if ($result->num_rows != 0) {
         returnWithError("Login already taken.");
         exit();
     }
@@ -64,13 +62,10 @@
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if( $row = $result->fetch_assoc()  )
-    {
+    if($row = $result->fetch_assoc()) {
         $retValue = array("id" => $row['ID'], "firstName" => $firstName, "lastName" => $lastName, "error" => "");
         sendResultInfoAsJson($retValue);    
-    }
-    else
-    {
+    } else {
         returnWithError("Failed to get user id: " . $stmt->error);
     }
 
@@ -79,8 +74,7 @@
     $conn->close();
 
     // function to get input data from request body
-    function getRequestInfo()
-    {
+    function getRequestInfo() {
         $json = file_get_contents('php://input');
         $data = json_decode($json, true);
     
@@ -93,18 +87,15 @@
     }
     
     // function to send JSON response
-    function sendResultInfoAsJson( $obj )
-    {
+    function sendResultInfoAsJson($obj) {
         header('Content-type: application/json');
         echo json_encode($obj);
     }
  
     // function to return error message as JSON
-    function returnWithError( $err )
-    {
+    function returnWithError($err) {
         $retValue = array("id" => 0, "firstName" => "", "lastName" => "", "error" => $err);
         sendResultInfoAsJson( $retValue );
     }
 
 ?>
-
